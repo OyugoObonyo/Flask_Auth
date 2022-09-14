f3d9dd798c9fd5010190ab833f52070bb4dfab2e
OyugoObonyo <nyikal1999@gmail.com>
2022-09-14 21:35:30 +0300

    Delete unused relations

--- app/models.py
+++ app/models.py
@@ -9,8 +9,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    reports = db.relationship('Report', backref='reporter', lazy=True)
-    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __init__(self, email, password):
         self.email = email
@@ -27,45 +25,3 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def encode_auth_token(self, user_id):
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            payload,
-            os.environ.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
-
-    @staticmethod
-    def decode_auth_token(auth_token):
-        try:
-            payload = jwt.decode(auth_token, os.environ.get('SECRET_KEY'))
-            return payload['sub']
-        except jwt.ExpiredSignatureError:
-            return 'Signature expired. Please log in again.'
-        except jwt.InvalidTokenError:
-            return 'Invalid token. Please log in again.'
-    
-    def __repr__(self):
-        return f"{self.email}"
-
-
-class BlacklistedToken(db.Model):
-    """
-    Token Model for storing JWT tokens
-    """
-    __tablename__ = 'blacklist_tokens'
-
-    id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(500), unique=True, nullable=False)
-    blacklisted_on = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, token):
-        self.token = token
-        self.blacklisted_on = datetime.datetime.now()
-
-    def __repr__(self):
-        return f'{self.token}'
