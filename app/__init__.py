c92d767607be6a8a6920f46bda3e7ce5d669ef27
OyugoObonyo <nyikal1999@gmail.com>
2022-09-18 16:20:14 +0300

    Load environment variable

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,17 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-
 db = SQLAlchemy()
 migrate = Migrate(render_as_batch=True)
 
 def create_app(config_class: Config):
+    load_dotenv()
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
-    load_dotenv()
-
+    
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
