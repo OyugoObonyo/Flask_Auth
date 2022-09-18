1274843f032771404c0f56255fb02249f8d06214
OyugoObonyo <nyikal1999@gmail.com>
2022-09-18 16:19:47 +0300

    Set testing variable to False

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+from re import T
 
 class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = False
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
