260d9168985fa9671bc93d4771b5331cb56a9b45
OyugoObonyo <nyikal1999@gmail.com>
2022-09-18 12:55:38 +0300

    Create application fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from app import create_app
+from app import create_app, db
 from app.models import User
 from config import TestingConfig
 import pytest
@@ -10,11 +10,23 @@ def app():
     return app
 
 
-@pytest.fixture()
-def user():
-    user = User(
-        email= "testuser@email.com",
-        password="testpassword"
-    )
-    return user
+@pytest.fixture(scope="session")
+def app_client():
+    app = create_app(config_class=TestingConfig)
+    client = app.test_client()
+    ctx = app.app_context()
+    ctx.push()
+    yield client
+    ctx.pop()
 
+@pytest.fixture(scope="session")
+def app_db():
+    db.create_all()
+    user = User(
+        email='user@mail.com',
+        password='user_password'
+    )
+    db.session.add(user)
+    db.session.commit()
+    yield db
+    db.drop_all()
