c29f03eb098392314314e9a5f268bf409028d1c0
OyugoObonyo <nyikal1999@gmail.com>
2022-09-20 21:53:46 +0300

    Test login routes

---
+++ tests/functional/test_login_routes.py
@@ -0,0 +1,10 @@
+def test_full_login(client):
+    json_data = {
+        "email" : "user2@mail.com",
+        "password": "password"
+    }
+
+def test_login_without_password(client):
+    json_data = {
+        
+    }
