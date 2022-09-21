c274182767e51cb6f0d107e2d912bec3aaff01d3
OyugoObonyo <nyikal1999@gmail.com>
2022-09-21 16:57:58 +0300

    Comment out login tests for now

--- tests/functional/test_login_routes.py
+++ tests/functional/test_login_routes.py
@@ -1,10 +1,10 @@
-def test_full_login(client):
-    json_data = {
-        "email" : "user2@mail.com",
-        "password": "password"
-    }
+# def test_full_login(client):
+#     json_data = {
+#         "email" : "user2@mail.com",
+#         "password": "password"
+#     }
 
-def test_login_without_password(client):
-    json_data = {
+# def test_login_without_password(client):
+#     json_data = {
         
-    }
+#     }
