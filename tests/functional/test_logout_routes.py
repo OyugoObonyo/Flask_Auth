b8428c3dfc5f3897a756c17728f322b2492410d5
OyugoObonyo <nyikal1999@gmail.com>
2022-09-23 07:23:24 +0300

    Test logout with expired token

--- tests/functional/test_logout_routes.py
+++ tests/functional/test_logout_routes.py
@@ -41,11 +41,17 @@ def test_logout_with_invalid_token(client):
     assert response.json["Status"] == "error"
     assert response.json["Message"] == "Invalid token"
 
-def test_logout_with_expired_token(client):
+def test_logout_with_expired_token(client, user):
+    login_response = client.post("/api/auth/login", json = {
+        "email" : "user@mail.com",
+        "password": "user_password"
+    })
+    token = login_response.json["Token"]
     headers = {
-        'Authorization': 'Bearer anInvalidToken'
+        'Authorization': f'Bearer {token}'
     }
-    response = client.post("/api/auth/logout", headers=headers)
-    assert response.status_code == 400
-    assert response.json["Status"] == "error"
-    assert response.json["Message"] == "Token expired. Please log in again"
+    client.post("/api/auth/logout", headers=headers)
+    repeat_logout = client.post("/api/auth/logout", headers=headers)
+    assert repeat_logout.status_code == 400
+    assert repeat_logout.json["Status"] == "error"
+    assert repeat_logout.json["Message"] == "Token expired. Please log in again"
