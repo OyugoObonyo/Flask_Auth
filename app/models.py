31d25be4446e40f336ded47faa891a2c0097ad6f
OyugoObonyo <nyikal1999@gmail.com>
2022-09-24 16:12:18 +0300

    Update fields in user model.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,22 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
+import uuid
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = email = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
+    public_id = db.Column(db.String(50), unique=True)
     password_hash = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __init__(self, email, password):
+    def __init__(self, username, email, password):
         self.email = email
+        self.username = username
+        self.public_id = uuid.uuid4().hex
         self.password_hash = self.set_password(password)
 
     @property
