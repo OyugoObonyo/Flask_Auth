6c14ec27b9e3009e1ff54f1cd1048a7d8c9656d0
OyugoObonyo <nyikal1999@gmail.com>
2022-09-28 06:01:24 +0300

    Add register without username test

--- tests/functional/test_registration_routes.py
+++ tests/functional/test_registration_routes.py
@@ -10,18 +10,29 @@ def test_full_registration(client):
 
 def test_register_without_email(client):
     response = client.post("/api/auth/register", json={
+        "username": "username",
         "password": "password"
     })
     assert response.status_code == 400
-    assert response.json["Message"] == "Email or password cannot be blank"
+    assert response.json["Message"] == "Email, username or password cannot be blank"
+    assert response.json["Status"] == "error"
+
+def test_register_without_username(client):
+    response = client.post("/api/auth/register", json={
+        "email": "user@mai.com",
+        "password": "password"
+    })
+    assert response.status_code == 400
+    assert response.json["Message"] == "Email, username or password cannot be blank"
     assert response.json["Status"] == "error"
 
 def test_register_without_password(client):
     response = client.post("/api/auth/register", json={
         "email" : "user2@mail.com",
+        "username": "username"
     })
     assert response.status_code == 400
-    assert response.json["Message"] == "Email or password cannot be blank"
+    assert response.json["Message"] == "Email, username or password cannot be blank"
     assert response.json["Status"] == "error"
 
 def test_register_already_existing_user(client, user):
