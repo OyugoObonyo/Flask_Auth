9e6d18b477d2cacf39a5b547d5646bd22c00972e
OyugoObonyo <nyikal1999@gmail.com>
2022-09-30 07:49:17 +0300

    Add Refresh configurations

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import os
 
 
@@ -5,6 +6,8 @@ class Config(object):
     # Set to True in production
     # JWT_COOKIE_SECURE = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=5)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TESTING = False
 
