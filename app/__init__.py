125576e6d32ae23865d06fd36f1d615fde5d30bf
OyugoObonyo <nyikal1999@gmail.com>
2022-10-08 10:41:15 +0300

    add app's urls

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_restful import Api
 
+load_dotenv()
 db = SQLAlchemy()
 jwt = JWTManager()
 migrate = Migrate(render_as_batch=True)
@@ -11,15 +13,19 @@ migrate = Migrate(render_as_batch=True)
 
 def create_app(config_class):
     app = Flask(__name__)
-    load_dotenv()
+    api = Api(app)
     app.config.from_object(config_class)
 
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
 
-    from app.auth import bp as auth_bp
+    import app.auth.views as auth_views
 
-    app.register_blueprint(auth_bp)
+    api.add_resource(auth_views.RegistrationView, '/register')
+    api.add_resource(auth_views.LoginView, '/login')
+    api.add_resource(auth_views.LogoutView, '/logout')
+    api.add_resource(auth_views.TokenRefreshView, '/refresh')
+    api.add_resource(auth_views.UserDetailView, '/me')
 
     return app
