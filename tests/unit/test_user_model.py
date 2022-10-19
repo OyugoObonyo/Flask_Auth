f1b3c2ee612d9da584bf7a8db891286f295af9f8
OyugoObonyo <nyikal1999@gmail.com>
2022-10-19 20:30:14 +0300

    add tests for password and username validation

--- tests/unit/test_user_model.py
+++ tests/unit/test_user_model.py
@@ -19,3 +19,31 @@ def test_save_user_to_db(user):
 def test_user_password(user):
     assert user.check_password("user_passworrd") is False
     assert user.check_password("user_password") is True
+
+
+@pytest.mark.parametrize(
+    "password", "output",
+    [
+        ("notavalidpassword", """
+        Password should be at least 6 characters long and must contain 1
+        """),
+        ("Astrong!Password001", "valid"),
+        ("S8ort", False),
+        ("NosPecialCharact8", True)
+    ]
+)
+def test_password_validity(user, password, output): 
+    assert user.validate_password(password) is output
+
+
+@pytest.mark.parametrize(
+    "username",  "output",
+    [
+        ("me", False),
+        ("User_99", True),
+        ("peter-98.py", True),
+        ("Notauser!", False)
+    ]
+)
+def test_username_validity(user, username, output):
+    assert user.validate_username(username) is output
